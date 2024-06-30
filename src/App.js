dc3533fcb9e31df9a9f783f5ad538ade1000a4fd
Leonardo Kremer <168785661+DevLeonardoK@users.noreply.github.com>
2024-06-30 17:06:41 -0300

    add portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ParticlesComponent from "./components/particles";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fullscreen">
+      <ParticlesComponent id="particles" />
+
+      <div className="container-info">
+        <h1 id="message-hello"> |HELLO|</h1>
+
+        <span id="message-welcome">WELCOME TO MY PORTFOLIO</span>
+      </div>
     </div>
   );
 }
