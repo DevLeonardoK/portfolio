dde53d8a03abb3bd72196d5d42f4ae17c9294c18
DevLeonardoK <devleonardok@gmail.com>
2024-07-01 17:29:24 -0300

    photo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import ParticlesComponent from "./components/particles";
 import "./App.css";
+import logophoto from "./assets/fotoPerfil.jpg";
 
 function App() {
   return (
@@ -10,6 +11,18 @@ function App() {
         <h1 id="message-hello"> |HELLO|</h1>
 
         <span id="message-welcome">WELCOME TO MY PORTFOLIO</span>
+      </div>
+
+      <div className="info-me">
+        <section id="photo-name">
+          <img src={logophoto} alt="logo" />
+        </section>
+        <section id="about-me">
+          <h2 id="tag">Full Stack Developer</h2>
+          <h3 id="others-tags">
+            Java, React, JavaScript, Html, Css, Sass, MySQL, SpringBoot
+          </h3>
+        </section>
       </div>
     </div>
   );
